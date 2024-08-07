79c84be1aa524a7c1f6a0278a3d6e7da54625782
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2024-08-07 16:36:02 -0700

    add gpt4vision

--- model.py
+++ model.py
@@ -20,6 +20,9 @@ def model(model_name, prompt):
     if(model_name == 'gpt4o'):
         from models.gpt_4o import gpt_4o
         return gpt_4o(prompt)
+    if(model_name == 'gpt4vision'):
+        from models.gpt_4_vision import gpt_4o_vision
+        return gpt_4o_vision(prompt)
     if(model_name == 'gpt4omini'):
         from models.gpt_4o_mini import gpt_4o_mini
         return gpt_4o_mini(prompt)
