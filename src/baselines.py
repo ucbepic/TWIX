a8a8a196aa343de162303418dbc61b40d5a88072
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2024-08-12 23:08:44 -0700

    add baseline evaluation

--- src/baselines.py
+++ src/baselines.py
@@ -1,8 +1,8 @@
 import sys
-import key,eval
+import key,eval,time
 sys.path.append('/Users/yiminglin/Documents/Codebase/Pdf_reverse/')
 from model import model 
-model_name = 'gpt4'
+model_name = 'gpt4o'
 
 def LLM_text(phrases, path):
     instruction = 'The following list contains keys and values extracted from a document, return all the keys seperated by |. Do not generate duplicated keys. Do not make up new keys.'
@@ -13,6 +13,26 @@ def LLM_text(phrases, path):
     #print(response)
     l = response.split('|')
     key.write_result(path, l)
+
+def LLM_table_extraction(phrases, path):
+    instruction = 'The following list contains keys and values extracted from a table, return the table. The first row is a set of keys, the following rows are the values corresponding to the keys, seperate keys and values by using comma. '
+    delimiter = ', '
+    context = delimiter.join(phrases)
+    prompt = (instruction,context)
+    response = model(model_name,prompt)
+    #print(response)
+    #l = response.split('|')
+    key.write_result(path, response)
+
+def LLM_KV_extraction(phrases, path):
+    instruction = 'The following list contains keys and values extracted from a document, find all the keys and their corresponding values. In each line, return a key and its value. Do not generate duplicated keys. Do not make up new keys.'
+    delimiter = ', '
+    context = delimiter.join(phrases)
+    #print(context)
+    prompt = (instruction,context)
+    response = model(model_name,prompt)
+    #print(response)
+    key.write_result(path, response)
 
 if __name__ == "__main__":
 
@@ -25,15 +45,24 @@ if __name__ == "__main__":
     tested_paths.append(root_path + '/data/raw/certification/MT/RptEmpRstrDetail Active.pdf')
     tested_paths.append(root_path + '/data/raw/certification/VT/Invisible Institue Report.pdf')
 
-
+    test_lst = [1]
+    
     for tested_id in range(len(tested_paths)):
 
+        if(tested_id not in test_lst):
+            continue
+        print(tested_id)
+        t1 = time.time()
         path = tested_paths[tested_id]
         print(path)
-        result_path = key.get_baseline_result_path(path,'textLLM_gpt4')
+        name = 'LLM_' + model_name + '_kv'
+        result_path = key.get_baseline_result_path(path,name)
         truth_path = key.get_truth_path(path,1)
         extracted_path = key.get_extracted_path(path)
 
         phrases = eval.read_file(extracted_path)
-        LLM_text(phrases, result_path)
+        #LLM_text(phrases, result_path)
+        LLM_KV_extraction(phrases, result_path)
+        t2 = time.time()
+        print(t2-t1)
     
