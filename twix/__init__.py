8cecf8937391bfbd0cadc3a15a0d8019360c8951
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-01-21 21:29:29 -0800

    update init by adding predict_template

--- twix/__init__.py
+++ twix/__init__.py
@@ -2,7 +2,8 @@
 
 from .key import predict_field
 from .pattern import extract_data
+from .pattern import predict_template
 from .extract import extract_phrase
 from .transform import transform
 
-__all__ = ["predict_field", "extract_data", "extract_phrase", "transform"]
+__all__ = ["predict_field", "predict_template", "extract_data", "extract_phrase", "transform"]
