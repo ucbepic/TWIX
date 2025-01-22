c0a334a0cf733d4b5245d130aad0090e3b40b536
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-01-21 16:25:06 -0800

    simplify models

--- twix/model.py
+++ twix/model.py
@@ -1,8 +1,5 @@
 #this is the models API. You pass the model (name of the model) and prompt, the API will return the response out 
 def model(model_name, prompt, image_path = ''):
-    if(model_name is 'gpt4'):
-        from models.gpt_4_model import gpt_4
-        return gpt_4(prompt)
     if(model_name == 'gpt4o'):
         from models.gpt_4o import gpt_4o
         return gpt_4o(prompt)
@@ -12,12 +9,6 @@ def model(model_name, prompt, image_path = ''):
     if(model_name == 'gpt4omini'):
         from models.gpt_4o_mini import gpt_4o_mini
         return gpt_4o_mini(prompt)
-    if(model_name is 'gpt4_azure'):
-        from models.gpt_4_azure import gpt_4_azure
-        return gpt_4_azure(prompt)
-    if(model_name is 'gpt4_long'):
-        from models.gpt_4_long import gpt_4_long
-        return gpt_4_long(prompt)
     return 'input model does not exist'
 
 
