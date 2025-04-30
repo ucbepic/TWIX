7e0fcb28570fc75544ee86e8d6cbb6349ccf2d3c
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-04-30 13:40:53 -0700

    add test runs for twix_transform

--- twix/transform.py
+++ twix/transform.py
@@ -2,13 +2,13 @@ from . import key, pattern, extract
 import os 
     
 #This is end-to-end APIs that directly extract data from raw documents
-def transform(pdf_paths, result_folder_path):
+def transform(pdf_paths, result_folder_path, LLM_model_name):
     total_cost = 0
-    phrases, cost = extract.extract_phrase(pdf_paths, result_folder_path)
+    phrases, cost = extract.extract_phrase(pdf_paths, result_folder_path, LLM_model_name=LLM_model_name)
     total_cost += cost 
-    fields, cost = key.predict_field(pdf_paths, result_folder_path)
+    fields, cost = key.predict_field(pdf_paths, result_folder_path, LLM_model_name=LLM_model_name)
     total_cost += cost 
-    template, cost = pattern.predict_template(pdf_paths, result_folder_path)
+    template, cost = pattern.predict_template(pdf_paths, result_folder_path, LLM_model_name=LLM_model_name)
     total_cost += cost 
     extraction_objects, cost = pattern.extract_data(pdf_paths, result_folder_path)
     total_cost += cost 
