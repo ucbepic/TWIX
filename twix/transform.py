3fcbdda587ee4846976d52b114a87287412922dd
yiminl18 <32020422+yiminl18@users.noreply.github.com>
2025-05-01 15:14:07 -0700

    add vision feature

--- twix/transform.py
+++ twix/transform.py
@@ -2,9 +2,9 @@ from . import key, pattern, extract
 import os 
     
 #This is end-to-end APIs that directly extract data from raw documents
-def transform(pdf_paths, result_folder_path, LLM_model_name):
+def transform(pdf_paths, result_folder_path, LLM_model_name, vision_feature = False):
     total_cost = 0
-    phrases, cost = extract.extract_phrase(pdf_paths, result_folder_path, LLM_model_name=LLM_model_name)
+    phrases, cost = extract.extract_phrase(pdf_paths, result_folder_path, LLM_model_name=LLM_model_name, vision_feature=vision_feature)
     total_cost += cost 
     fields, cost = key.predict_field(pdf_paths, result_folder_path, LLM_model_name=LLM_model_name)
     total_cost += cost 
